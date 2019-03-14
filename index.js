0eaaf5375ccf27b668588003495818eacdf159e1
Diego Vega Silva <diegovsil@central.inditex.grp>
2019-03-14 16:00:51 +0100

    simple notification

--- index.js
+++ index.js
@@ -3,22 +3,48 @@
 
     const applicationServerPublicKey = 'BDSY9OtPoOFFq8XpQNhnX_c2ZVRH_OZSaG5Ku9NPwagPYKylVxKs9QQSV5iIQ6jpHzcYYccoSrt-ZW7Yo6vVxDg';
 
-    let swRegistration;
 
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-        console.log('Service Worker and Push is supported');
-      
-        navigator.serviceWorker.register('sw.js')
-        .then(function(swReg) {
-          console.log('Service Worker is registered', swReg);
-      
-          swRegistration = swReg;
-        })
-        .catch(function(error) {
-          console.error('Service Worker Error', error);
-        });
-      } else {
-        console.warn('Push messaging is not supported');
-        pushButton.textContent = 'Push Not Supported';
-      }
+
+    class PushSubscription {
+
+
+        constructor(publicKey) {
+            this.publicKey = publicKey;
+        }
+
+
+        init() {
+            this.swRegistration().then(registration => {
+                console.log('Service Worker is registered', registration);
+                this.pushSubscribe(registration)
+            }).catch(function(error) {});
+        }
+
+        swRegistration() {
+            return new Promise((resolve, reject) => {
+                navigator.serviceWorker.register('sw.js').then(registration => {
+                    resolve(registration);
+                }).catch(error => {
+                    reject(error)
+                })
+            })
+        }
+
+        pushSubscribe(registration) {
+            registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                /* applicationServerKey: this.applicationServerKey */
+            }).then(subscription => {
+              registration.showNotification('Hola Mundo');
+                console.log(subscription.endpoint);
+            }).catch(error => {
+                console.error('subscription error', error)
+            })
+        }
+    }
+
+
+    var Sub = new PushSubscription(applicationServerPublicKey)
+
+    Sub.init()
 })()
