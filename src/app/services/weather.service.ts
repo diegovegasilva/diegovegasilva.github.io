da65cb923348af73011fd6faebeeb9395d1e71f4
Diego Vega <diegovegasilva@gmail.com>
2019-03-16 08:29:47 +0100

    lodash added

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
+import _ from 'lodash';
+
 @Injectable({
   providedIn: 'root'
 })
--- src/app/shared/weather-card/weather-card.component.ts
+++ src/app/shared/weather-card/weather-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'PWA-weather-weather-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-card.component.scss']
 })
 export class WeatherCardComponent implements OnInit {
-
+  @Input() weatherData;
   constructor() { }
 
   ngOnInit() {
