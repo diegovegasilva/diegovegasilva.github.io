13ef723459cb5fd8d2a551704cf4e67faab5e09c
Diego Vega <diegovegasilva@gmail.com>
2019-03-16 10:14:58 +0100

    switch to metric units

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -13,7 +13,7 @@ export class WeatherService {
 
   get5DayForecast(cityId) {
     let url = 'https://api.openweathermap.org/data/2.5/forecast?id=';
-    return this.http.get(`${url}${cityId}&appid=${this.APIkey}`).pipe(
+    return this.http.get(`${url}${cityId}&appid=${this.APIkey}&units=metric`).pipe(
       map(res => {
         this.filterByDay(res);
         return res;
