f360bad10efb740a77ce6c535a1edd4f2d9cf0be
Diego Vega <diegovegasilva@gmail.com>
2019-03-17 13:12:43 +0100

    add ES locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { CitySelectorComponent } from './shared/city-selector/city-selector.component';
+
+
 
 @NgModule({
   declarations: [
@@ -21,8 +26,13 @@ import { CitySelectorComponent } from './shared/city-selector/city-selector.comp
     CoreModule,
     SharedModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es' } ],
   entryComponents : [ CitySelectorComponent ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+  constructor(){
+    registerLocaleData(localeEs, 'es');
+  }
+ }
