5a7b940d48879f77fbd8e05b75a98efb4f921f50
Diego Vega <diegovegasilva@gmail.com>
2019-03-17 22:09:18 +0100

    storing data on local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MatDialog } from '@angular/material';
 import { CitySelectorComponent } from './shared/city-selector/city-selector.component';
@@ -13,18 +13,33 @@ import _ from 'lodash';
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'PWA-weather-app';
-  citiesForeCast: Array<object> = initialCity;
-  activeCities: number[] = [initialCity[0].city.id];
+  citiesForeCast: Array<object> = [];
+  activeCities: any[];
   loading: boolean = true;
 
 
   constructor(
     private weatherService: WeatherService,
     public dialog: MatDialog
-  ) {}
+  ) {
+    if(localStorage.getItem('selectedCities')){
+      let activeIds = _.map(localStorage.getItem('selectedCities').split(','), id => parseInt(id));
+      this.activeCities  = activeIds;
+    } else {
+      this.activeCities = []
+    }
+  }
 
+  ngOnInit(){
+    if(this.activeCities.length > 0){
+      this.getCitiesForecast(this.activeCities)
+    } else{
+      this.activeCities = [initialCity[0].city.id];
+      this.citiesForeCast = initialCity;
+    }
+  }
   openDialog(): void {
     const dialogRef = this.dialog.open(CitySelectorComponent, {
       width: '250px'
@@ -47,23 +62,25 @@ export class AppComponent{
   }
 
   removeCity(cityId) {
-    _.remove(this.citiesForeCast, city => city.city.id === cityId);
-    _.remove(this.activeCities, city => city === cityId);
+    _.remove(this.citiesForeCast, city => parseInt(city.city.id) === parseInt(cityId));
+    _.remove(this.activeCities, city => parseInt(city) === parseInt(cityId));
+    console.log(this.activeCities)
+    localStorage.setItem('selectedCities', this.activeCities.join());
   }
 
   addCityData(cityId) {
     this.loading = true;
     this.weatherService.get5DayForecast(cityId).subscribe((data: any) => {
       this.loading = false;
-      let cityIndex = _.findIndex(
-        this.citiesForeCast,
-        city => city.city.id === data.city.id
-      );
+      console.log(this.activeCities, data.city.id)
+      let cityIndex = _.indexOf(this.activeCities, data.city.id)
+      console.log('cityIndex', cityIndex, cityId);
       if (cityIndex > -1) {
         this.citiesForeCast.splice(cityIndex, 1, data);
       } else {
         this.citiesForeCast.push(data);
         this.activeCities.push(data.city.id);
+        localStorage.setItem('selectedCities', this.activeCities.join())
       }
     });
   }
